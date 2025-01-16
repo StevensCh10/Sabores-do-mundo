9daa1aaf3888e3d33cfb6333f802fec64abacaa9
StevensCh10 <stevensch10@outlook.com>
2025-01-16 13:27:40 -0300

    feat: Adiciona pasta com excecoes pre-configuradas e outra pasta com personalizacao das excecoes

---
+++ Backend/src/main/java/com/saboresdomundo/backend/exception/CannotBeScheduling.java
@@ -0,0 +1,12 @@
+package com.saboresdomundo.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class CannotBeScheduling extends RuntimeException{
+    
+    public CannotBeScheduling(String message){
+        super(message);
+    }
+}
---
+++ Backend/src/main/java/com/saboresdomundo/backend/exception/EntityInUse.java
@@ -0,0 +1,12 @@
+package com.saboresdomundo.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EntityInUse extends RuntimeException{
+	
+	public EntityInUse(String message) {
+		super(message);
+	}
+}
