51d8029c1544ad37006d040ec5a7ad96a7858908
StevensCh10 <stevensch10@outlook.com>
2025-01-30 17:15:51 -0300

    feat: Adiciona métodos na classe ReserController

--- Backend/src/main/java/com/saboresdomundo/backend/controller/ReserveController.java
+++ Backend/src/main/java/com/saboresdomundo/backend/controller/ReserveController.java
@@ -1,8 +1,15 @@
 package com.saboresdomundo.backend.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.saboresdomundo.backend.model.Reserve;
 import com.saboresdomundo.backend.service.ReserveService;
 import lombok.RequiredArgsConstructor;
 
@@ -12,4 +19,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReserveController {
     private final ReserveService reserveService;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public Reserve createReserve(Reserve newReserve){
+        return reserveService.createReserve(newReserve);
+    }
+
+    @PutMapping
+    public Reserve alterNumberReservant(Reserve reserve, String number){
+        return reserveService.alterNumberReservant(reserve, number);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteReserve(Long id){
+        reserveService.excludeReserve(id);
+    }
 }
