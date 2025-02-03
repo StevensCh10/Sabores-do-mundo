e77fa6c9cf15a98ba88a38ee4d14e3ce307687cd
Stevens <stevensch10@outlook.com>
2025-02-03 17:03:26 -0300

    feat: Remove todas as classes java que nao serao mais necessarias na aplicacao
    Obs: Aplicacao so vai lidar com a reserva.

--- Backend/src/main/java/com/saboresdomundo/backend/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.saboresdomundo.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.saboresdomundo.backend.model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long>{
-    
-}
